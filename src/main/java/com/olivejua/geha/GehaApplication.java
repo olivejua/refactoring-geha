71642c5297a515b00763315a69e961cd6b684fd5
olivejua <tmfrl4710@gmail.com>
2021-06-17 15:18:36 +0900

    Initial commit

---
+++ src/main/java/com/olivejua/geha/GehaApplication.java
@@ -0,0 +1,13 @@
+package com.olivejua.geha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GehaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GehaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/olivejua/geha/GehaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olivejua.geha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GehaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
